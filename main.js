e48a5f7cca7ee4b57d20b8f5f3dc0566347374e7
valuelan <332716137@qq.com>
2019-12-20 14:28:08 +0800

    no message

---
+++ main.js
@@ -0,0 +1,8 @@
+const output = require('./output')
+const arr = require('./m3u8list.json');
+(async function () {
+  for (let i = 0; i < arr.length; i++) {
+    let [name, url] = arr[i].split('$');
+    await output(url, name + '.mp4');
+  }
+})();
